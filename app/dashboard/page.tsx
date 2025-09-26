71239a8bb85da4a888dffdf949c702f7da0322fb
jjrmrcly79 <juanjose.garces@gmail.com>
2025-09-26 14:26:11 -0600

    fgdfg

--- app/dashboard/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function DashboardMirror() {
-  return (
-    <div style={{padding:16}}>
-      <h1 style={{fontSize:24,fontWeight:700}}>Dashboard (mirror app/)</h1>
-    </div>
-  );
-}
